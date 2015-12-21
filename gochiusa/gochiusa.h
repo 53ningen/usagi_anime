8a75b9ff1781d02c93f6b7d83165421491d1d84b
53nigen <korun24276@gmail.com>
2015-12-21 14:04:01 +0900

    init

---
+++ gochiusa/gochiusa.h
@@ -0,0 +1,18 @@
+//
+//  gochiusa.h
+//  gochiusa
+//
+//  Copyright © 2015年 gomi_ningen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for gochiusa.
+FOUNDATION_EXPORT double gochiusaVersionNumber;
+
+//! Project version string for gochiusa.
+FOUNDATION_EXPORT const unsigned char gochiusaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <gochiusa/PublicHeader.h>
+
+
---
+++ gochiusa/ご注文はうさぎですか.swift
@@ -0,0 +1,11 @@
+//
+//  ご注文はうさぎですか.swift
+//  gochiusa
+//
+//  Copyright © 2015年 gomi_ningen. All rights reserved.
+//
+
+import Foundation
+
+public class ご注文はうさぎですか {
+}
